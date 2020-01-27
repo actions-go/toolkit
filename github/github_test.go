1a53c08d48e85cc34a0082c2f64c8ccd6c65c37d
Thibault Jamet <tjamet@users.noreply.github.com>
2020-01-27 22:25:53 +0100

    Add a helper to download files from a repository (#10)

--- github/github_test.go
+++ github/github_test.go
@@ -2,11 +2,17 @@ package github_test
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/actions-go/toolkit/github"
 	"github.com/stretchr/testify/assert"
 )
+
+const content = `package toolkit
+
+// placeholder for go docs
+`
 
 func TestClient(t *testing.T) {
 	repo, _, err := github.GitHub.Repositories.Get(context.Background(), "actions", "toolkit")
@@ -14,4 +20,16 @@ func TestClient(t *testing.T) {
 	assert.NotNil(t, repo.Owner)
 	assert.NotNil(t, repo.Owner.Login)
 	assert.EqualValues(t, "actions", *repo.Owner.Login)
+}
+
+func TestDownload(t *testing.T) {
+	files := github.DownloadSelectedRepositoryFiles(http.DefaultClient, "actions-go", "toolkit", "09edac1c7d93e0dd7fe5a14dc410fb0b41ea01c4", github.MatchesOneOf("^module.go$"))
+	assert.Equal(t, map[string][]byte{"module.go": []byte(content)}, files)
+}
+
+func TestMatchOneOf(t *testing.T) {
+	assert.True(t, github.MatchesOneOf("\\.github/settings\\..*")(".github/settings.json"))
+	assert.False(t, github.MatchesOneOf("\\.github/settings\\..*")(".github/settings/branches/master/protection.json"))
+	assert.True(t, github.MatchesOneOf("\\.github/settings\\..*", ".github/settings/.*")(".github/settings/branches/master/protection.json"))
+	assert.False(t, github.MatchesOneOf("\\.github/some-other.*")(".github/settings/branches/master/protection.json"))
 }
